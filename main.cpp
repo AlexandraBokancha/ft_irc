08e69773c84a04ce5e17cec1125473348bbf1658
Alex <bokanchaalexandra@gmail.com>
2024-11-11 19:55:14 +0100

    fixing main

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ stream-sockets are reliable two-way connected communication stream */
 
 /* TCP makes sure your data arrives sequentially and error-free */
 
+/* Port number is a 16-bit number that's like the local address for the connection
+All ports below 1024 are RESERVED, you can use a port up to 65535 */
+
 /* non-blocking calls don't block the thread until they finish
 their work; instead, this type of system call returns immediately
 after completing its job without having any effect on what's happening
 in other threads */
-
-
 
 
 int main(int ac, char *av[]){
@@ -22,36 +23,44 @@ int main(int ac, char *av[]){
     /* AF_INET = IPv4 protocol, SOCK_STREAN = TCP type socket*/
 
     Server irc(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
-   
-    /* defining server address */
 
-    sockaddr_in serverAddress; // struct is used to represent an IPv4 address and port number combination
+    /* defining server address */
+    struct sockaddr_in serverAddress; // struct is used to represent an IPv4 address and port number combination
 
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(port); // convert the int from machine byte to network byte
+    serverAddress.sin_port = htons(port); // convert the int from machine (host) byte to network byte
     serverAddress.sin_addr.s_addr = INADDR_ANY; // listen to all ips
+    memset(&(serverAddress.sin_zero), '\0', sizeof(serverAddress.sin_zero));
+   
+    /* to prevent bind() from failed when a socket that was connected
+     is still hanging around in the kernel, and it's hogging the port */
+    int yes = 1;
+    if (setsockopt(irc.getSocket(), SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){
+        std::cout << "Error: setsockopt() failed" << std::endl;
+        exit(-1);
+    }
 
-/* when you first create the socket descriptor with socket(),
-   the kernel sets it to blocking. If you don't want a socket
-   to be blocking, you have to make a call to fcntl()
+    /* when you first create the socket descriptor with socket(),
+    the kernel sets it to blocking. If you don't want a socket
+    to be blocking, you have to make a call to fcntl()
 
     * fcntl () -> performs operations on the open fd
     * F_SETFL -> set the file status flags to the value specified by arg
     * O_NONBLOCK -> the bit that enables nonblocking mode for the file.
-*/
-    if (fcntl(irc.getSocket(), F_SETFL, O_NONBLOCK) < 0){
+    */
+    if (fcntl(irc.getSocket(), F_SETFL, O_NONBLOCK) == -1){
         std::cout << "Error: failed to set a non-blocking flag for the socket" << std::endl;
         exit(-1); // < remplacer par l'exception ?>
     }
 
     /* binding the server socket */
-    if (bind(irc.getSocket(), (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+    if (bind(irc.getSocket(), (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1){
         std::cout << "Error: socket connection failed" << std::endl;
         exit(-1); // < remplacer par l'exception ?>
     }
 
     /* listening for connections */
-    if (listen(irc.getSocket(), SOMAXCONN) < 0){
+    if (listen(irc.getSocket(), SOMAXCONN) == -1){
         std::cout << "Error: listen() falied" << std::endl;
         exit(-1); // < remplacer par l'exception ?>
     }
@@ -72,7 +81,7 @@ int main(int ac, char *av[]){
 
     /* accepting a client connection */
     for (;;){
-        if (poll(&(irc.getFds()), irc.getFdsSize(), -1) < 0){
+        if (poll(&(irc.getFds()), irc.getFdsSize(), -1) == -1){
             std::cout << "Error: poll() failed";
             exit(-1);
         }
